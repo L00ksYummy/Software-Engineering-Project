c1ce727772b97cdb3910f835285d3a9f14cc84de
L00ksYummy <michaelanthony2004@gmail.com>
2025-12-13 18:25:16 -0500

    Refactor database operations and improve input handling

--- SoftwareEngineeringApp.java
+++ SoftwareEngineeringApp.java
@@ -1,5 +1,5 @@
-/*Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
-*/
+// Software Engineering Project main file
+// Michael Allen, Kyle Pollack, Colin Williams
 
 /*
  * list patient id and name then let them pick it
@@ -23,21 +23,29 @@ public class SoftwareEngineeringApp {
 
         Database db = new Database(url, user, password);
         Connection conn = DriverManager.getConnection(url, user, password);
+
+        // creating hashmap for easy checking and running of methods
         HashMap<String, Runnable> operations = new HashMap<>();
         operations.put("S", db.search(conn, scan));
+        operations.put("A", db.add(conn, scan));
+        operations.put("D", db.delete(conn, scan));
+        operations.put("U", db.update(conn, scan));
 
         System.out.println("Hello! This is the database for our Optometry Practice.\n");
 
-        db.printData(conn, "Select Last, 'First Name', MRN from patients");
+        db.printData(conn, "Select Last, `First Name`, MRN from patients");
 
         String userInput = "";
 
+        // Valid input check and call of the 4 functions
         while (true) {
+            userInput = "";
             System.out.println(
                     "Would you like to Search(S), Update(U), Delete(D), Add(A) database? (e to cancel process)");
-            userInput = scan.next();
+            userInput = scan.next().toUpperCase();
 
-            if (userInput.equalsIgnoreCase("E")) {
+            if (userInput.equals("E")) {
+                System.out.println("Ending process...");
                 break;
             } else if (!operations.containsKey(userInput)) {
                 System.out.println("Please Enter one of the 4 options (S, U, D, A, or E)!");
@@ -49,4 +57,5 @@ public class SoftwareEngineeringApp {
 
         scan.close();
     }
+
 }
